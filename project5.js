675b6bbade3b03dea053da132b4ee29a9337e0d7
Sayantan <maitysayantan111@gmail.com>
2023-09-04 07:29:28 +0530

    project5 done

--- project5.js
+++ project5.js
@@ -12,11 +12,23 @@ var arr = [
 
 
 var clutter = "";
+var storiyan =document.querySelector("#storiyan"); 
 arr.forEach(function (elem,idx) {
    clutter +=`<div class="story">
    <img id="${idx}" src="${elem.dp}" alt="" srcset="">
  </div>`
 });
 
-document.querySelector("#storiyan").innerHTML = clutter;
+storiyan.innerHTML = clutter;
+
+storiyan.addEventListener("click",function(dets){
+  document.querySelector("#full-screen").style.display = "block";
+  document.querySelector("#full-screen").style.backgroundImage = `url(${arr[dets.target.id].story})`;
+  
+setTimeout(function(){
+  document.querySelector("#full-screen").style.display = "none";
+},3000);
+
+});
+
 
